7d49463cb35d9d1b376f1325c03a82bac33fba07
Samsudeen_SM <samsudeen.mohammed@oneteamus.com>
2024-01-31 21:46:12 +0530

    WIP

--- src/modules/user/user.modal.js
+++ src/modules/user/user.modal.js
@@ -17,7 +17,7 @@ const RSA_PUBLIC_KEY = fs.readFileSync(path.join(__dirname, './../../../keys/jwt
 
 // Database configuration
 export const sequelize = new Sequelize('harizon_travels', 'root', 'Shamsudeen123%', {
-    host: '::1',
+    host: 'localhost',
     dialect: 'mysql',
     port: 3306
   });
