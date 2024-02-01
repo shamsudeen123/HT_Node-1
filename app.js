91ab6e11ccd040ae461bf0a4dfdd254d2f5b15a8
Samsudeen_SM <samsudeen.mohammed@oneteamus.com>
2024-02-01 19:18:49 +0530

    WIP

--- app.js
+++ app.js
@@ -25,4 +25,7 @@ sequelize.sync().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
 });
