a24d15165ae07c3f9adb99730dc9d14657ac066f
Samsudeen_SM <samsudeen.mohammed@oneteamus.com>
2024-02-01 21:20:38 +0530

    WIP

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import jwt from "jsonwebtoken";
 import apiRouter from "./src/router.js"
-// import { sequelize } from "./src/modules/user/user.modal.js";
+import { sequelize } from "./src/modules/user/user.modal.js";
 import path from "path";
 import { fileURLToPath } from 'url';
 import http from 'http';
-import { Sequelize, DataTypes } from 'sequelize';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -21,18 +20,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 //for static file loading like images
 // app.use('/uploads', express.static('uploads/userProfile'));
 
-export const sequelize = new Sequelize('harizon_travels', 'root', 'Shamsudeen123%', {
-  host: 'localhost',
-  dialect: 'mysql',
-  // port: 3306
-});
-
 
 sequelize.sync().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
-})
-.catch(err => {
-  console.error('Unable to connect to the database:', err);
 });
--- src/modules/user/user.modal.js
+++ src/modules/user/user.modal.js
@@ -9,7 +9,7 @@ import bcrypt from 'bcrypt';
 
 // Database configuration
 export const sequelize = new Sequelize('harizon_travels', 'root', 'Shamsudeen123%', {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'mysql',
   // port: 3306
 });
